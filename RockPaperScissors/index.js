5ce4258035d38d90844f36688a99be144dc34b9a
david <hall.davidwv@gmail.com>
2023-09-11 13:01:31 +0800

    Add function playRound in index.js
    Added function playRound in index.js. The purpose of the function is to play a single round of Rock, Paper Scissors and declares who is the winner.

--- RockPaperScissors/index.js
+++ RockPaperScissors/index.js
@@ -1,5 +1,31 @@
 function getComputerChoice () {
     let choice = ["Rock", "Paper", "Scissors"];
     let randomValue = Math.floor(Math.random() * choice.length);
-    return console.log(choice[randomValue]);
+    return choice[randomValue];
 }
+
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper") {
+        return "You lose! Paper beats Rock.";
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") {
+        return "You win! Rock beats Scissors.";
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "rock") {
+        return "Draw! You and the Computer chose Rock."
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors") {
+        return "You lose! Scissors beats Paper.";
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") {
+        return "You win! Paper beats Rock.";
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "paper") {
+        return "Draw! You and the Computer chose Paper."
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock") {
+        return "You lose! Rock beats Scissors.";
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper") {
+        return "You win! Scissors beat Paper.";
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "paper") {
+        return "Draw! You and the Computer chose Paper."
+    }
+}
+
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
