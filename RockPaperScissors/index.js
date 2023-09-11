899e81d3b941c5779c2509de0ef8da1cc5522596
david <hall.davidwv@gmail.com>
2023-09-11 13:29:37 +0800

    Add function game in index.js
    Added function game in index.js. The purpose of this function is to simulate the game, Rock Paper Scissors using prompt method for the user input and while loop for the rounds in the function.

--- RockPaperScissors/index.js
+++ RockPaperScissors/index.js
@@ -4,28 +4,62 @@ function getComputerChoice () {
     return choice[randomValue];
 }
 
-function playRound (playerSelection, computerSelection) {
+function playRound (playerSelection, computerSelection, playerScore, computerScore) {
     if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper") {
+        computerScore++;
+        console.log("Player Score: ", playerScore);
+        console.log("Computer Score: ", computerScore);
         return "You lose! Paper beats Rock.";
     } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") {
+        playerScore++;
+        console.log("Player Score: ", playerScore);
+        console.log("Computer Score: ", computerScore);
         return "You win! Rock beats Scissors.";
     } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "rock") {
-        return "Draw! You and the Computer chose Rock."
+        console.log("Player Score: ", playerScore);
+        console.log("Computer Score: ", computerScore);
+        return "Draw! You and the Computer chose Rock.";
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors") {
+        computerScore++;
+        console.log("Player Score: ", playerScore);
+        console.log("Computer Score: ", computerScore);
         return "You lose! Scissors beats Paper.";
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") {
+        playerScore++;
+        console.log("Player Score: ", playerScore);
+        console.log("Computer Score: ", computerScore);
         return "You win! Paper beats Rock.";
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "paper") {
-        return "Draw! You and the Computer chose Paper."
+        console.log("Player Score: ", playerScore);
+        console.log("Computer Score: ", computerScore);
+        return "Draw! You and the Computer chose Paper.";
     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock") {
+        computerScore++;
+        console.log("Player Score: ", playerScore);
+        console.log("Computer Score: ", computerScore);
         return "You lose! Rock beats Scissors.";
     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper") {
+        playerScore++;
+        console.log("Player Score: ", playerScore);
+        console.log("Computer Score: ", computerScore);
         return "You win! Scissors beat Paper.";
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "paper") {
-        return "Draw! You and the Computer chose Paper."
+        console.log("Player Score: ", playerScore);
+        console.log("Computer Score: ", computerScore);
+        return "Draw! You and the Computer chose Paper.";
     }
 }
 
-const playerSelection = "Rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    let round = 0;
+    let playerScore = 0;
+    let computerScore = 0;
+    while (round < 5) {
+        playerSelection = window.prompt("Player: ");
+        computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection, playerScore, computerScore));
+        round++;
+    }
+}
+
+game();
